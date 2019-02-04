64ce5d79eb585211d3ccd377f679eb3a003a9713
esharri2 <esharri2@gmail.com>
2019-02-04 14:18:25 -0500

    port config

--- keystone.js
+++ keystone.js
@@ -1,37 +1,34 @@
-var keystone = require('keystone');
-var handlebars = require('express-handlebars');
-require('dotenv').config()
-
+var keystone = require("keystone");
+var handlebars = require("express-handlebars");
+require("dotenv").config();
 
 //Read about more options here:
 //https://keystonejs.com/documentation/configuration/
 keystone.init({
-    'cookie secret': process.env.KEYSTONESECRET,
-    // 'cookie secret': "test",
-    'name': 'ladyliberty',
-    'sass': 'public',
-	'static': 'public',
-    'user model': 'User',
-    'auto update': true,
-    'auth': true,
-    'mongo': process.env.MONGO_URI || "mongodb://localhost/prodco",
-    'views': 'templates/views',
-    'view engine': '.hbs',
-    'custom engine': handlebars.create({
-        layoutsDir: 'templates/views/layouts',
-        partialsDir: 'templates/views/partials',
-        defaultLayout: 'default',
-        helpers: new require('./templates/views/helpers')(),
-        extname: '.hbs',
-    }).engine,
-    'cloudinary config': process.env.CLOUDINARY_URL
+  "cookie secret": process.env.KEYSTONESECRET,
+  // 'cookie secret': "test",
+  name: "ladyliberty",
+  sass: "public",
+  static: "public",
+  "user model": "User",
+  "auto update": true,
+  auth: true,
+  port: process.env.PORT,
+  mongo: process.env.MONGO_URI || "mongodb://localhost/prodco",
+  views: "templates/views",
+  "view engine": ".hbs",
+  "custom engine": handlebars.create({
+    layoutsDir: "templates/views/layouts",
+    partialsDir: "templates/views/partials",
+    defaultLayout: "default",
+    helpers: new require("./templates/views/helpers")(),
+    extname: ".hbs"
+  }).engine,
+  "cloudinary config": process.env.CLOUDINARY_URL
 });
 
+keystone.set("routes", require("./routes"));
 
-
-
-keystone.set('routes', require('./routes'));
-
-keystone.import('models');
+keystone.import("models");
 
 keystone.start();
