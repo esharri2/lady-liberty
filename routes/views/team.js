2943a814fa5a1413c6e53396c934a9a9aafb5934
esharri2 <esharri2@gmail.com>
2019-02-04 12:42:30 -0500

    fixed modal with tall img

--- routes/views/team.js
+++ routes/views/team.js
@@ -1,34 +1,33 @@
-var keystone = require('keystone');
-var async = require('async');
+var keystone = require("keystone");
+var async = require("async");
 
-module.exports = function (req, res) {
+module.exports = function(req, res) {
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+  locals.section = "team";
 
-    var view = new keystone.View(req, res);
-    var locals = res.locals;
-    locals.section = 'team';
+  var Person = keystone.list("Person");
 
-    var Person = keystone.list('Person');
+  view.on("init", function(next) {
+    var q = Person.model.find().populate("category");
+    q.exec(function(err, results) {
+      const sortedTeam = results.sort((a, b) => {
+        if (a.order > b.order) {
+          return 1;
+        } else if (a.order < b.order) {
+          return -1;
+        }
+      });
+      sortedTeam.forEach((person, index) => {
+        if (person.bio !== "" || person.image.secure_url) {
+          person.showMoreButton = true;
+        }
+      });
+      console.log(sortedTeam);
+      locals.everyone = sortedTeam;
+      next(err);
+    });
+  });
 
-    view.on('init', function (next) {
-        var q = Person.model.find().populate('category');
-        q.exec(function (err, results) {
-            const sortedTeam = results.sort((a, b) => {
-                if (a.order > b.order) {
-                    return 1;
-                } else if (a.order < b.order) {
-                    return -1;
-                }
-            })
-            sortedTeam.forEach((person, index) => {
-                if (person.bio !== "" || person.image.secure_url) {
-                    person.showMoreButton = true;
-                }
-            })
-            locals.everyone = sortedTeam;
-            next(err);
-        })
-    })
-
-
-    view.render('team');
+  view.render("team");
 };
