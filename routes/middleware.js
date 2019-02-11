f53486ba68c510ffab17a5a3901cc560b00bf77c
esharri2 <esharri2@gmail.com>
2019-02-10 19:27:57 -0500

    changed nav link

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,13 +1,13 @@
-exports.initLocals = function (req, res, next) {
-	res.locals.navLinks = [
-		{ label: 'Home', key: 'index', href: '/' },
-		{ label: 'The Story', key: 'about', href: '/about' },
-		{ label: 'Video', key: 'video', href: '/video' },
-		{ label: 'Cast and crew', key: 'team', href: '/team' },
-		{ label: 'Behind the scenes', key: 'images', href: '/images' },
-		{ label: 'Contact', key: 'contact', href: '/contact' },
-	];
-	res.locals.companyName = "Lady Liberty"
-	res.locals.user = req.user;
-	next();
+exports.initLocals = function(req, res, next) {
+  res.locals.navLinks = [
+    { label: "Home", key: "index", href: "/" },
+    { label: "The Story", key: "about", href: "/about" },
+    { label: "Trailer", key: "video", href: "/video" },
+    { label: "Cast and crew", key: "team", href: "/team" },
+    { label: "Behind the scenes", key: "images", href: "/images" },
+    { label: "Contact", key: "contact", href: "/contact" }
+  ];
+  res.locals.companyName = "Lady Liberty";
+  res.locals.user = req.user;
+  next();
 };
